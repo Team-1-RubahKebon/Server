30d8afebe332dfabbca22c988c41c072fa33a6e7
JCCode288 <jendycaprico@gmail.com>
2023-03-29 17:02:07 +0700

    feat: add association feat: add mock_data

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 module.exports = class Controller {
-  static async home(req, res, next) {}
+  static async home(req, res, next) {
+    try {
+      let { id } = req.params;
+      if (!id) {
+        throw new Errors(400, "Id must be filled");
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
 };
--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const multer = require("multer");
 
 router.get("/", Controller.home);
+router.get("/login/teacher");
+router.get("/login/student");
 
 module.exports = router;
