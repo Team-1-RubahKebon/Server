66418d365775bb4bac0e2eb8548edffb77a04d97
JCCode288 <jendycaprico@gmail.com>
2023-03-30 23:32:46 +0700

    feat: add route register teacher

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,11 @@
 const Controller = require("../controllers");
 const router = require("express").Router();
 const studentsRouter = require("./student");
+const routerTeacher = require("./teacher");
 
 router.get("/", Controller.home);
 
 router.use("/students", studentsRouter);
+router.use("/teachers", routerTeacher);
 
 module.exports = router;
--- routers/teacher.js
+++ routers/teacher.js
@@ -3,5 +3,6 @@ const TeacherController = require("../controllers/TeacherController");
 const routerTeacher = require("express").Router();
 
 routerTeacher.post("/login", TeacherController.login);
+routerTeacher.post("/register", TeacherController.register);
 
 module.exports = routerTeacher;
