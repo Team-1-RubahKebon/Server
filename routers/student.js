d44baaf557684174de70e42ed94d5210822505c9
JCCode288 <jendycaprico@gmail.com>
2023-03-31 23:37:07 +0700

    refactor: using populate when get data in students controller

--- routers/student.js
+++ routers/student.js
@@ -2,6 +2,7 @@ const StudentController = require("../controllers/StudentController");
 const { upload } = require("../middlewares/multer");
 const studentsRouter = require("express").Router();
 
+studentsRouter.get("/", StudentController.getStudents);
 studentsRouter.post("/login", StudentController.login);
 studentsRouter.post("/googlein", StudentController.googleLogin);
 studentsRouter.post("/register", StudentController.register);
@@ -10,9 +11,8 @@ studentsRouter.post(
   upload.single("file"),
   StudentController.recognizing
 );
-studentsRouter.get("/", StudentController.getStudents);
-studentsRouter.get("/:id", StudentController.getStudentById);
 studentsRouter.get("/assignments", StudentController.getAssignments);
+studentsRouter.get("/:id", StudentController.getStudentById);
 studentsRouter.get("/assignments/:id", StudentController.getAssignmentById);
 
 module.exports = studentsRouter;
