8fc5fdf46f7e93bdb6d89255047e3dbbaea09429
linggarrehand <linggarrehand@gmail.com>
2023-03-31 11:22:47 +0700

    feat: create teacher-create-class

--- routers/teacher.js
+++ routers/teacher.js
@@ -7,6 +7,7 @@ routerTeacher.post("/googlein", TeacherController.googleLogin);
 routerTeacher.post("/register", TeacherController.register);
 routerTeacher.post("/googleLogin", TeacherController.googleLogin);
 routerTeacher.get("/class", TeacherController.getClass);
+routerTeacher.post("/class", TeacherController.createClass);
 routerTeacher.get("/assignments", TeacherController.getAssignments);
 routerTeacher.post("/assignments", TeacherController.createAssignment);
 
