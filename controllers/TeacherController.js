00f4be3ddec99f04b228f878bba3753eab90cacc
linggarrehand <linggarrehand@gmail.com>
2023-04-03 02:36:08 +0700

    fix: fix teacher testing and teacher controller

--- controllers/TeacherController.js
+++ controllers/TeacherController.js
@@ -105,7 +105,13 @@ module.exports = class TeacherController {
 
   static async getClasses(req, res, next) {
     try {
-      let allClass = await Class.find({})
+      let name = req.query.name;
+      let query = {};
+
+      if (name) {
+        query.name = { $regex: `${name}` };
+      }
+      let allClass = await Class.find(query)
         .populate("Assignments")
         .populate("Teacher")
         .populate("Students");
@@ -130,7 +136,7 @@ module.exports = class TeacherController {
         query.name = { $regex: `${name}` };
       }
 
-      let assignments = await Assignment.find(query);
+      let assignments = await Assignment.find(query).populate("ClassId");
 
       res.status(200).json(assignments);
     } catch (err) {
@@ -145,7 +151,8 @@ module.exports = class TeacherController {
 
       let assignmentById = await Assignment.findOne({ _id })
         .populate("ClassId")
-        .populate("StudentAnswers");
+        .populate("StudentAnswers")
+        .populate("QuestionId");
 
       res.status(200).json(assignmentById);
     } catch (err) {
